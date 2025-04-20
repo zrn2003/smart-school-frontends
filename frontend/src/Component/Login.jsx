bb531288876c14dc3f64e30e65255d587a467ec1
zrn2003 <zishanrn2003@gmail.com>
2025-04-20 10:21:41 +0530

    update

--- frontend/src/Component/Login.jsx
+++ frontend/src/Component/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
         e.preventDefault();
         if (username && password) {
             try {
-                const response = await fetch('http://localhost:5000/api/auth/login', {
+                const response = await fetch('https://smart-school-backend-2.onrender.com/api/auth/login', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
