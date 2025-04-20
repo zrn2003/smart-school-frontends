261b7873877451ad427af7d4a4e17c9c05b45ca9
Zishan Nadaf <zishanrn2003@gmail.com>
2025-04-20 10:51:31 +0530

    Update Login.jsx

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:8055/login', {
+      const response = await fetch('https://smart-school-backend-2.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
